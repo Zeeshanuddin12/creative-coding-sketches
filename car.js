6079efccab590b1d3ee2c2ccd35749ad35c3a140
Zeeshanuddin12 <mohammadzeeshanuddin128@gmail.com>
2025-06-01 01:05:14 +0400

    car.js

---
+++ car.js
@@ -0,0 +1,34 @@
+
+function setup() {
+    createCanvas(400, 400);
+    background(220);
+  
+    // Car body (main rectangle)
+    fill(0, 0, 139); // dark blue
+    rect(100, 200, 200, 50, 15); // x, y, w, h, rounded corners
+  
+    // Car cabin (rectangle)
+    fill(0, 0, 205); // medium blue
+    rect(130, 150, 140, 50);
+  
+    // Car top (semi-circle)
+    fill(0, 0, 205); // medium blue
+    arc(200, 150, 140, 70, PI, 0);
+  
+    // Windows
+    fill(173, 216, 230); // light blue
+    rect(140, 160, 30, 30);
+    rect(230, 160, 30, 30);
+  
+    // Wheels
+    fill(0); // black
+    ellipse(150, 250, 50, 50);
+    ellipse(250, 250, 50, 50);
+  
+    // Wheel hubs
+    fill(169, 169, 169); // grey
+    ellipse(150, 250, 20, 20);
+    ellipse(250, 250, 20, 20);
+  }
+  
+  
